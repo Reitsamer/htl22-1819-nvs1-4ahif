daa5b935d5d697b135dee9a33623fefb524bdce7
Wolf Reitsamer <wr@elmar21.com>
2019-03-05 14:29:38 +0100

    First exported function.

--- node-notes/app.js
+++ node-notes/app.js
@@ -3,3 +3,5 @@ console.log('Starting app.js');
 const notes = require('./notes');
 
 console.log(notes);
+
+console.log('Call from app.js:', notes.add(11, 22));
--- node-notes/notes.js
+++ node-notes/notes.js
@@ -1,5 +1,17 @@
 console.log('Starting notes.js');
 
+var name = "Franzi";
+var adresse = {
+  plz: 1190,
+  ort: "Wien"
+};
+
+module.exports.add = function(x, y) {
+  return x + y;
+}
+
+// console.log('Ergebnis:', add(5, 7));
+
 module.exports.age = 25;
 
 // console.log(module);
