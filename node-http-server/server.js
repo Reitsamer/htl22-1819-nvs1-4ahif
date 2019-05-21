94786fcfbf274cb41d97e028dcd6add9ac236bbb
Wolf Reitsamer <reit@htl-donaustadt.at>
2019-05-21 15:34:21 +0200

    working static http server

--- node-http-server/server.js
+++ node-http-server/server.js
@@ -1,4 +1,12 @@
 const http = require('http')
+const fs = require('fs')
+
+const contentTypes = {
+    'html': 'text/html',
+    'js': 'application/javascript',
+    'css': 'text/css',
+    'ico': 'image/x-icon'
+}
 
 const server = http.createServer()
 
@@ -6,12 +14,34 @@ server.on('request', (request, response) => {
     console.log('Url:', request.url)
     console.log('Method:', request.method)
 
+    const fileName = getFilename(request)
+    const fileExtension = getFileExtension(fileName)
+    const contentType = getContentType(fileExtension)
+
     response.statusCode = 200
-    response.setHeader('Content-Type', 'text/html')
-    response.write('<h1>Hello world!</h1>')
+    response.setHeader('Content-Type', contentType)
+    const content = fs.readFileSync(fileName)
+    response.write(content)
     response.end()
 })
 
 server.listen(8080, () => {
     console.log('Server running on port 8080.')
 })
+
+const getFilename = (request) => {
+    var filename = request.url
+    if (filename === '/')
+        filename = '/index.html'
+
+    return 'public' + filename
+}
+
+const getFileExtension = (filename) => {
+    var parts = filename.split('.')
+    return parts[parts.length - 1]
+}
+
+const getContentType = (extension) => {
+    return contentTypes[extension]
+}
